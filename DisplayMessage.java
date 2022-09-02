7c17a9721c637bd8d146fe6fda74711836b6a0f3
KOUSHIK PRAMANIK <110465853+Koushik16pramanik@users.noreply.github.com>
2022-09-02 14:56:39 +0530

    Update DisplayMessage.java

--- DisplayMessage.java
+++ DisplayMessage.java
@@ -1,11 +1,8 @@
 package Activity;
 
 public class DisplayMessage {
-		DisplayMessage() {
-			System.out.println("Hallo My Message");
-		}
-	public static void main(String[] args) {
-		DisplayMessage d=new DisplayMessage();
+		void printMessage() {
+		System.out.println("Hello my message");
 
 	}
 
