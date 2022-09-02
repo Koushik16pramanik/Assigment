2b0c6a9f4a17ea993cc87373d93b2f7352d6bc63
KOUSHIK PRAMANIK <110465853+Koushik16pramanik@users.noreply.github.com>
2022-09-02 09:59:52 +0530

    Add files via upload

---
+++ DisplayMessage.java
@@ -0,0 +1,12 @@
+package Activity;
+
+public class DisplayMessage {
+		DisplayMessage() {
+			System.out.println("Hallo My Message");
+		}
+	public static void main(String[] args) {
+		DisplayMessage d=new DisplayMessage();
+
+	}
+
+}
